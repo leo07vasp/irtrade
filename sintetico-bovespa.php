de45558b99afd7d06ef9f36073040dfe8651fe06
Lucas Almeida <lucas@perdidao.net>
2012-11-19 23:41:08 -0200

    Sintetico Bovespa pronto

---
+++ sintetico-bovespa.php
@@ -0,0 +1,231 @@
+<?php include('header.php'); ?>
+	<div class="sinteticos-bovespa" id="main">
+		<div class="title-content-default">
+			<h1>Sintético Bovespa</h1>
+			<span class="desc">Nos dados iniciais você irá inserir blabla bla blablablablabla bla bla bla bla bla bla.</span>
+		</div>	
+
+		<p class="text-apresentcao">Utilize o filtro abaixo para verificar sua carteira em um determinado dia. Isso ajuda você a ter um maior controle de suas operações em datas passadas.<br />
+Caso você queira apenas saber o que esta em aberto em sua carteira selecione apenas o tipo de negocio desejado.</p>
+
+
+		<div class="search-data">
+			<div class="select-data">
+			<label for="tipoData">Data Base</label>
+			<select name="TipoData" id="tipoData">
+				<option value="data_base">bmf</option>
+				<option value="data1">Data 1</option>
+				<option value="data2">Data 2</option>
+				<option value="data3">Data 3</option>
+				<option value="data4">Data 4</option>
+			</select>
+			</div>
+			<button class="big-buscar">buscar</button>
+		</div>
+
+		<div class="bovespa-container">
+			<div class="tabs sinteticos-bovespa-content">	
+
+				<ul class="links">
+					<li class="active"><a href="#info">Info</a> </li>
+					<li><a href="#jan">jan</a> </li>
+					<li><a href="#fev">fev</a> </li>
+					<li><a href="#fev">fev</a> </li>
+					<li><a href="#mar">mar</a> </li>
+					<li><a href="#abr">abr</a> </li>
+					<li><a href="#mai">mai</a> </li>
+					<li><a href="#jun">jun</a> </li>
+					<li><a href="#jul">jul</a> </li>
+					<li><a href="#ago">ago</a> </li>
+					<li><a href="#set">set</a> </li>
+					<li><a href="#out">out</a> </li>
+					<li><a href="#nov">nov</a> </li>
+					<li><a href="#dez">dez</a> </li>
+				</ul>
+
+				<div class="conteudos">
+
+					<div id="info">
+
+						<div class="result-sintetico">
+							<p class="mercado">Mercado: Vista</p>
+							
+							<table width="100%" cellspacing="0" class="data-table">
+							  <thead>
+							    <th scope="col" class="bg-fix">&nbsp;</th>
+							    <th scope="col" class="bg-fix">&nbsp;</th>
+							    <th colspan="5" scope="col">Data Liquidação</th>
+							    <th colspan="5" scope="col">Data Liquidação</th>
+							    <th colspan="2" scope="col">Resutados</th>
+							  </thead>
+							  <tbody>
+								  <tr class="bg-fix">
+								    <td>Corretora</td>
+								    <td>Ativo</td>
+								    <td>Data</td>
+								    <td>C/V</td>
+								    <td>Quant.</td>
+								    <td>Preço</td>
+								    <td>Total c/ Taxas</td>
+								    <td>Data</td>
+								    <td>C/V</td>
+								    <td>Quant.</td>
+								    <td>Preço</td>
+								    <td>Total c/ Taxas</td>
+								    <td>Comum</td>
+								    <td>DayTrade</td>
+								  </tr>
+								  <tr>
+								    <td>Safra Corretora</td>
+								    <td>Bradesco LSG D11</td>
+								    <td>12/12/12</td>
+								    <td>C</td>
+								    <td>999000</td>
+								    <td>R$ 99,00</td>
+								    <td>R$ 4.470.942,20</td>
+								    <td>12/12/12</td>
+								    <td>C</td>
+								    <td>999000</td>
+								    <td>R$ 99,00</td>
+								    <td>R$ 4.470.942,20</td>
+								    <td>R$ 44.470.942,20</td>
+								    <td>R$ 44.470.942,20</td>
+								  </tr>
+								  <tr>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								  </tr>
+								  <tr>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								  </tr>
+								  <tr>
+								    <td colspan="12" class="t-right">Totais</td>
+								    <td>R$ 44.470.942,20</td>
+								    <td>R$ 44.470.942,20</td>
+								  </tr>
+								</tbody>
+							</table>
+						</div>
+
+						<div class="result-sintetico">
+							<p class="mercado">Mercado: Vista</p>
+							
+							<table width="100%" cellspacing="0" class="data-table">
+							  <thead>
+							    <th scope="col" class="bg-fix">&nbsp;</th>
+							    <th scope="col" class="bg-fix">&nbsp;</th>
+							    <th colspan="5" scope="col">Data Liquidação</th>
+							    <th colspan="5" scope="col">Data Liquidação</th>
+							    <th colspan="2" scope="col">Resutados</th>
+							  </thead>
+							  <tbody>
+								  <tr class="bg-fix">
+								    <td>Corretora</td>
+								    <td>Ativo</td>
+								    <td>Data</td>
+								    <td>C/V</td>
+								    <td>Quant.</td>
+								    <td>Preço</td>
+								    <td>Total c/ Taxas</td>
+								    <td>Data</td>
+								    <td>C/V</td>
+								    <td>Quant.</td>
+								    <td>Preço</td>
+								    <td>Total c/ Taxas</td>
+								    <td>Comum</td>
+								    <td>DayTrade</td>
+								  </tr>
+								  <tr>
+								    <td>Safra Corretora</td>
+								    <td>Bradesco LSG D11</td>
+								    <td>12/12/12</td>
+								    <td>C</td>
+								    <td>999000</td>
+								    <td>R$ 99,00</td>
+								    <td>R$ 4.470.942,20</td>
+								    <td>12/12/12</td>
+								    <td>C</td>
+								    <td>999000</td>
+								    <td>R$ 99,00</td>
+								    <td>R$ 4.470.942,20</td>
+								    <td>R$ 44.470.942,20</td>
+								    <td>R$ 44.470.942,20</td>
+								  </tr>
+								  <tr>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								  </tr>
+								  <tr>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								    <td>&nbsp;</td>
+								  </tr>
+								  <tr>
+								    <td colspan="12" class="t-right">Totais</td>
+								    <td>R$ 44.470.942,20</td>
+								    <td>R$ 44.470.942,20</td>
+								  </tr>
+								</tbody>
+							</table>
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<br style="clear:both;" />
+		</div>
+
+	</div>
+
+<?php include('footer.php'); ?>

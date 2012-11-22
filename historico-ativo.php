537400b6b156ead0e0ea26e6e49ae8db0d5564f1
Leonardo Rodrigues <leonardorodrigues@MacBook-Pro-de-Leonardo.local>
2012-11-22 21:28:41 -0200

    historico ativo feito

--- historico-ativo.php
+++ historico-ativo.php
@@ -2,10 +2,119 @@
 
 <div id="main" class="historico-ativo">
 			<div class="title-content-default">
-			<h1>histórico ativo</h1>
-			<span class="desc">Nos dados iniciais você irá inserir blabla bla blablablablabla bla bla bla bla bla bla.</span>
-		</div>		
+				<h1>histórico ativo</h1>
+				<span class="desc">Nos dados iniciais você irá inserir blabla bla blablablablabla bla bla bla bla bla bla.</span>
+			</div>		
 
+	<div class="search-data">
+		
+			<label for="periodo">
+				<span>Periodo</span>
+					<input style="width:55px;" type="text"/>
+						<span>a</span>
+					<input style="width:55px;" type="text"/>
+			</label>
+				
+			<label for="nomeAtivo">
+				<span>Nome do Ativo</span>
+					<input style="width:100px;" type="text"/>
+				
+			</label>					
+				<br style="clear:both;">
+					<label for="tipomercado">
+						<span>Tipo de Mercado</span>
+							<select style="width:100px;" name="tipomercado" id="">
+								<option value="">Selecione</option>
+								<option value="">data 1</option>
+							</select>
+						
+					</label>
+						<label for="observacao">
+							<span>Observação</span>
+						
+							<select style="width:100px;" name="observacao" id="">
+								<option value="">selecione</option>
+								<option value="">data1</option>
+							</select>
+						</label>
+					
+				
+			<button class="big-buscar">Buscar</button>
+				
+			
+		
+	</div>
+
+<h2>nome do ativo:petr3</h2>
+	<hr class="blue-sep"/>
+		<h3>ano base:2009</h3>
+			
+				
+					<table id="historico-ativo" width="930" class="data-table data-zebrada data-hover">
+						<thead>
+							
+								<th>Corretora</th>
+								<th>Numero Nota</th>
+								<th>Data Pregão</th>
+								<th>Mercado</th>
+								<th>Observação</th>
+								<th>Compra</th>
+								<th>Quantidade</th>
+								<th>Preço / Dia</th>
+								<th>Estoque</th>
+								<th>Preço / Estoque</th>
+							
+						</thead>
+							
+								
+									<tbody>
+										
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+										
+									</tbody>
+								
+							
+						
+					</table>
+				
+			
+		
+	
 
 
 

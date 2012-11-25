4500f6e3b60ecba2260617377cc4bf8f2cbbdf3c
Lucas Almeida <lucas@perdidao.net>
2012-11-25 17:51:59 -0200

    Fundos Imobiliários e botoes de pdf, darf e xls com classes responsivas

--- fundos-imobiliarios.php
+++ fundos-imobiliarios.php
@@ -2,14 +2,122 @@
 	
 		
 			<div id="main" class="fundos-imobiliarios">
-			<div class="title-content-default">
-				<h1>fundos imobiliários</h1>
-				<span class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore assumenda. </span>
-			</div>	
+				<div class="title-content-default">
+					<h1>fundos imobiliários</h1>
+					<span class="desc">Phasellus venenatis nibh sed risus pellentesque ut commodo erat pharetra, nunc et malesuada consequat, neque sapien posuere dui. </span>
+				</div>	
 					
-						
+				<div class="search-data">
+					<div class="select-data">
+					<label for="tipoData">Data Base</label>
+					<select name="TipoData" id="tipoData">
+						<option value="selecione">Selecione</option>
+						<option value="data1">Data 1</option>
+						<option value="data2">Data 2</option>
+						<option value="data3">Data 3</option>
+						<option value="data4">Data 4</option>
+					</select>
+					</div>
+					<button class="big-buscar">buscar</button>
+				</div>
+
+				<br style="clear:both;" />
 			
-					
+				<p class="ganhos">
+					<span>GANHOS LÍQUIDOS OU PERDAS</span>
+					<a href="#" class="bt-pdf"></a>
+					<a href="#" class="bt-xls"></a>
+				</p>
+				
+				<table width="100%" cellspacing="0" class="data-table">
+					<thead >
+						<th scope="col">Mês</th>
+						<th scope="col">Resultado Líquido<br /> do mês</th>
+						<th scope="col">Resultado negativo até<br /> o mês anterior</th>
+						<th scope="col">Base de cálculo<br /> do imposto</th>
+						<th scope="col">Prejuízo a<br /> compensar</th>
+						<th scope="col">Alíquota do<br /> imposto</th>
+						<th scope="col">Imposto devido</th>
+						<th scope="col">Darf a pagar</th>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="bg-fix">Janeiro</td>
+							<td>R$ 1.258,00</td>
+							<td>R$ 1.258,00</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td><a href="#" class="bt-darf"></a></td>
+						</tr>
+						<tr>
+							<td class="bg-fix">Fevereiro</td>
+							<td>R$ 1.258,00</td>
+							<td>R$ 1.258,00</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td><a href="#" class="bt-darf"></a></td>
+						</tr>
+						<tr>
+							<td class="bg-fix">Março</td>
+							<td>R$ 1.258,00</td>
+							<td>R$ 1.258,00</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>R$ 1.258,00</td>
+							<td><a href="#" class="bt-darf"></a></td>
+						</tr>
+						<tr>
+							<td class="bg-fix">Abril</td>
+							<td>R$ 1.258,00</td>
+							<td>R$ 1.258,00</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>R$ 1.258,00</td>
+							<td><a href="#" class="bt-darf"></a></td>
+						</tr>
+						<tr>
+							<td class="bg-fix">Maio</td>
+							<td>R$ 1.258,00</td>
+							<td>R$ 1.258,00</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>R$ 1.258,00</td>
+							<td><a href="#" class="bt-darf"></a></td>
+						</tr>
+						<tr>
+							<td class="bg-fix">Junho</td>
+							<td>R$ 1.258,00</td>
+							<td>R$ 1.258,00</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td><a href="#" class="bt-darf"></a></td>
+						</tr>
+						<tr>
+							<td class="bg-fix">Julho</td>
+							<td>R$ 1.258,00</td>
+							<td>R$ 1.258,00</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td><a href="#" class="bt-darf"></a></td>
+						</tr>
+					</tbody>
+				</table>
+
+				<div class="button-set">
+					<a href="#" class="bt-pdf"></a>
+					<a href="#" class="bt-xls"></a>
+				</div>
 				
 			</div>
 		

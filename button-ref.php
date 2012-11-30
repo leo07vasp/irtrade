7ad6a7955357d6340930f9061ff33c563ba0b1fd
Lucas Almeida <lucas@perdidao.net>
2012-11-30 00:08:57 -0200

    Botoes com classe, hover e disabled pelo sprite novo

---
+++ button-ref.php
@@ -0,0 +1,151 @@
+<?php include('header.php'); ?>
+
+<div id="main">
+	<div class="title-content-default">
+		<h1>Referência de botões</h1>
+	</div>		
+
+	<ul class="bt-reference">
+		<li>
+			<span>bt-cancelar</span>
+			<a href="#" class="bt-cancelar"></a>
+		</li>
+		<li>
+			<span>bt-cancelar-disabled</span>
+			<a href="#" class="bt-cancelar-disabled"></a>
+		</li>
+		<li>
+			<span>bt-salvar</span>
+			<a href="#" class="bt-salvar"></a>
+		</li>
+		<li>
+			<span>bt-salvar-disabled</span>
+			<a href="#" class="bt-salvar-disabled"></a>
+		</li>
+		</li>
+		<li>
+			<span>bt-buscar</span>
+			<a href="#" class="bt-buscar"></a>
+		</li>
+		<li>
+			<span>bt-buscar-disabled</span>
+			<a href="#" class="bt-buscar-disabled"></a>
+		</li>
+		<li>
+			<span>bt-gerar</span>
+			<a href="#" class="bt-gerar"></a>
+		</li>
+		<li>
+			<span>bt-gerar-disabled</span>
+			<a href="#" class="bt-gerar-disabled"></a>
+		</li>
+		<li>
+			<span>bt-enviar</span>
+			<a href="#" class="bt-enviar"></a>
+		</li>
+		<li>
+			<span>bt-enviar-disabled</span>
+			<a href="#" class="bt-enviar-disabled"></a>
+		</li>
+		<li>
+			<span>bt-alterar</span>
+			<a href="#" class="bt-alterar"></a>
+		</li>
+		<li>
+			<span>bt-alterar-disabled</span>
+			<a href="#" class="bt-alterar-disabled"></a>
+		</li>
+		<li>
+			<span>seta-direita</span>
+			<a href="#" class="seta-direita"></a>
+		</li>
+		<li>
+			<span>seta-direita-disabled</span>
+			<a href="#" class="seta-direita-disabled"></a>
+		</li>
+		<li>
+			<span>seta-esquerda</span>
+			<a href="#" class="seta-esquerda"></a>
+		</li>
+		<li>
+			<span>seta-esquerda-disabled</span>
+			<a href="#" class="seta-esquerda-disabled"></a>
+		</li>
+		<li>
+			<span>buscar-mini</span>
+			<a href="#" class="buscar-mini"></a>
+		</li>
+		<li>
+			<span>buscar-mini-disabled</span>
+			<a href="#" class="buscar-mini-disabled"></a>
+		</li>
+		<li>
+			<span>bt-darf</span>
+			<a href="#" class="bt-darf"></a>
+		</li>
+		<li>
+			<span>bt-darf-disabled</span>
+			<a href="#" class="bt-darf-disabled"></a>
+		</li>
+		<li>
+			<span>bt-xls</span>
+			<a href="#" class="bt-xls"></a>
+		</li>
+		<li>
+			<span>bt-xls-disabled</span>
+			<a href="#" class="bt-xls-disabled"></a>
+		</li>
+		<li>
+			<span>bt-pdf</span>
+			<a href="#" class="bt-pdf"></a>
+		</li>
+		<li>
+			<span>bt-pdf-disabled</span>
+			<a href="#" class="bt-pdf-disabled"></a>
+		</li>
+		<li>
+			<span>bt-lupa</span>
+			<a href="#" class="bt-lupa"></a>
+		</li>
+		<li>
+			<span>bt-lupa-disabled</span>
+			<a href="#" class="bt-lupa-disabled"></a>
+		</li>
+		<li>
+			<span>bt-excluir</span>
+			<a href="#" class="bt-excluir"></a>
+		</li>
+		<li>
+			<span>bt-excluir-disabled</span>
+			<a href="#" class="bt-excluir-disabled"></a>
+		</li>
+		<li>
+			<span>bt-checked</span>
+			<a href="#" class="bt-checked"></a>
+		</li>
+		<li>
+			<span>bt-checked-disabled</span>
+			<a href="#" class="bt-checked-disabled"></a>
+		</li>
+		<li>
+			<span>big-mais</span>
+			<a href="#" class="big-mais"></a>
+		</li>
+		<li>
+			<span>big-mais-disabled</span>
+			<a href="#" class="big-mais-disabled"></a>
+		</li>
+		<li>
+			<span>big-menos</span>
+			<a href="#" class="big-menos"></a>
+		</li>
+		<li>
+			<span>big-menos-disabled</span>
+			<a href="#" class="big-menos-disabled"></a>
+		</li>
+		<br style="clear:both;" />
+	</ul>
+
+</div>
+
+<?php include('footer.php'); ?>
